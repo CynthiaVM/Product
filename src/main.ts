6959b79a791ccbaea90105e6fcd6970ce4a34b02
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-05-08 21:48:33 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { getProductById, getProducts } from "../src/service/Produc";
 
-const listProductos = await getProducts();
 
+const listProductos = await getProducts();
 console.log(listProductos);
 
 const producto = await getProductById(1);
-
 console.log(producto);
