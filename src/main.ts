ac2b46fcb25b9ec886110e7a717233cb9fd3a0be
CynthiaVM <80722354+CynthiaVM@users.noreply.github.com>
2023-05-08 21:47:25 -0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { getProductById, getProducts } from "../src/service/Produc";
+
+const listProductos = await getProducts();
+
+console.log(listProductos);
+
+const producto = await getProductById(1);
+
+console.log(producto);
---
+++ src/service/Produc.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { product } from "../Interfaces/Product.Interfaces";
+
+export async function getProducts() {
+	const response = await axios.get("https://fakestoreapi.com/products");
+	const products: product[] = response.data;
+	return products;
+}
+
+export async function getProductById(id: number) {
+	const response = await axios.get(`https://fakestoreapi.com/products/${id}`);
+	const product: product = response.data;
+	return product;
+}
